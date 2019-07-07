ccb0f47c68e9ee72b3e6f68b96af367a1bc116e9
Allan Assis <allan.andrade@corp.globo.com>
2019-07-07 18:06:14 -0300

    feat: add jwt support and ecrypt

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,67 @@
 var express = require("express");
 var User = require("../models/User");
+var jwt = require("jsonwebtoken");
+var bcrypt = require("bcrypt");
 
 var router = express.Router();
 
 /* GET users listing. */
-router.get("/", function(req, res, next) {
-  User.find({}, (err, docs) => {
+router.get("/auth", function(req, res, next) {
+  let { authentication } = req.headers;
+  authentication = authentication.split(" ")[1];
+  jwt.verify(authentication, "teste", (err, decode) => {
     if (err) {
       console.log(err);
       res.send({ status: 500, msg: "ruim" });
     }
-    res.send({ status: 200, data: docs });
+    res.send({ status: 200, data: decode });
+  });
+});
+
+router.get("/", function(req, res, next) {
+  const { email, password } = req.body;
+  User.find({ email }, (err, docs) => {
+    if (err || !docs[0]) {
+      res.send({ status: 500, msg: "ruim" });
+    } else {
+      const doc = docs[0];
+
+      bcrypt
+        .compare(password, doc.password)
+        .then(match => {
+          if (match) {
+            const token = jwt.sign({ name: doc.name, email }, "teste", {
+              expiresIn: "24h"
+            });
+            res.send({ status: 200, data: { token, name: doc.name, email } });
+          } else {
+            res.send({ status: 500, msg: "ruim" });
+          }
+        })
+        .catch(err => {
+          res.send({ status: 500, msg: "ruim" });
+        });
+    }
   });
 });
 
 router.post("/", function(req, res, next) {
   const { name, email, password } = req.body;
-  const UserInstance = new User({ name, email, password });
-  UserInstance.save(err => console.log(err));
-  res.send({ status: 201, msg: req.body });
+  bcrypt.genSalt(10).then(salt =>
+    bcrypt.hash(password, salt).then(hashpass => {
+      const UserInstance = new User({ name, email, password: hashpass });
+      UserInstance.save((err, doc) => {
+        if (err) {
+          console.log(err);
+          res.send({ status: 500, msg: "error" });
+        }
+        const token = jwt.sign({ name, email }, "teste", {
+          expiresIn: "24h"
+        });
+        res.send({ status: 201, data: { token, name, email } });
+      });
+    })
+  );
 });
 
 module.exports = router;
